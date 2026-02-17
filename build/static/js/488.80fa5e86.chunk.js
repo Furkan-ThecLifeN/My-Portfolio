a2a978d9db0e5d2c02ec0af0ffd98f8dc355f1d7
Furkan-ThecLifeN <furkantheclifen@gmail.com>
2026-02-17 18:26:10 +0300

    vercel build successful

---
+++ build/static/js/488.80fa5e86.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio_website=self.webpackChunkportfolio_website||[]).push([[488],{2488(e,s,t){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.80fa5e86.chunk.js.map
